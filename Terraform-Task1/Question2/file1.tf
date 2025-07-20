98c42151a4afe6090ac5eff3fdfdeee9e5661022
Siddu-28design <siddusteli08353@gmail.com>
2025-07-19 23:36:00 -0700

    this is my terraform task1

---
+++ Terraform-Task1/Question2/file1.tf
@@ -0,0 +1,5 @@
+resource "local_file" "file1" {
+  filename = "file1.txt"
+  content  = "This is content from file1.tf"
+}
+
---
+++ Terraform-Task1/Question2/file3.tf
@@ -0,0 +1,4 @@
+resource "local_file" "file3" {
+  filename = "file3.txt"
+  content  = "This is content from file3.tf"
+}
